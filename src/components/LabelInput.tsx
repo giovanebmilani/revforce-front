8f707d92eb7516543c95ae04a7e3ec095a3efe29
Arthur Igansi <pizzolatti.arthur@edu.pucrs.br>
2025-03-30 19:36:52 -0300

    refactor: update LabelInput component to use a dedicated interface for props

--- src/components/LabelInput.tsx
+++ src/components/LabelInput.tsx
@@ -1,12 +1,17 @@
 import { Input } from "./ui/input";
 
-function LabelInput ( { inputTitle, props}: { inputTitle: string, props: React.ComponentProps<"input"> }) {
-    return (
-        <div>
-            <h3 className="font-medium">{inputTitle}</h3>
-            <Input {...props} />
-        </div>
-    )
+interface LabelInputProps {
+  inputTitle: string;
+  props?: React.ComponentProps<"input">;
+}
+
+function LabelInput({ inputTitle, props }: LabelInputProps) {
+  return (
+    <div>
+      <h3 className="font-medium">{inputTitle}</h3>
+      <Input {...props} />
+    </div>
+  );
 }
 
 export default LabelInput;
