23bfc51e1de890cdc9ce717e48e4fab896adc9dd
Felipe Valiati <felipe.cvaliati@gmail.com>
2025-03-30 21:08:49 -0300

    feat componente paper finalizado

--- src/components/paper.tsx
+++ src/components/paper.tsx
@@ -4,21 +4,22 @@ function Paper() {
     <div className="flex justify-center items-center w-full h-full">
       <div className="shadow-md rounded-lg w-1/3 h-1/4 outline-1">
         <div className="h-1/5 outline-1 rounded-t-lg">
-          <h2 className="text-xl font-semibold mb-2 text-l">Charts</h2>
+          <h2 className="font-semibold h-full grid items-center ml-2">Charts</h2>
         </div>
 
         <div className="h-4/5 w-full flex flex-row justify-center">
-          <div className="bg-red-500 h-full w-7/8">
-            graficos
+          <div className=" h-full w-7/8">
+            {/* TODO: botar os grafico aq */}
           </div>
 
-          <div className="bg-purple-400 w=1/8 text-2xl flex align-middle justify-center items-center">
-            <h1 className="rounded-lg outline-1 h-1/2 w-full">{'>'}</h1>
+          <div className="w-1/8 text-2xl flex align-middle justify-center items-center">
+            <svg className="p-2.5 rounded-lg outline-1 h-1/2 w-1/2 cursor-pointer" width="16" height="29" viewBox="0 0 16 29" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M2 26.5L14 14.5L2 2.5" stroke="#D1D1D1" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
           </div>
         </div>
       </div>
     </div>
-    
   );
 }
 
