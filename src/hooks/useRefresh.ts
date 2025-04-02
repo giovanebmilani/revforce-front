328c2f625e05ad41d2c168a10a7d9caa0b4a24f1
Erick <erickcarpesmmc@gmail.com>
2025-04-02 19:34:30 -0300

    leve correção na integração de refresh, é preciso esperar outras tasks

--- src/hooks/useRefresh.ts
+++ src/hooks/useRefresh.ts
@@ -1,18 +1,24 @@
+import { get } from "http";
+
 const API_BASE_URL = "http://localhost:8000"; // REVISAR URL 
 
-function useRefresh () {
+function useRefresh ({id}: {id: string}) {
 
     const fetchNextRefreshTime = async () => {
         try{
-            const response = await fetch(`${API_BASE_URL}/refresh-time`); // REVISAR ENDPOINT
+            const response = await fetch(`${API_BASE_URL}/refresh/:${id}`, {method: 'GET'}); // REVISAR ID
             if (!response.ok) {
-                throw new Error('Erro ao buscar o tempo de atualização de dados');
+                throw new Error('Erro ao buscar o próximo tempo de atualização de dados');
             }
 
             const data = await response.json();
 
-            // o que fazer com os dados recebidos??
-            return data.refresh_time; // ou o que for necessário
+            if (!data || !data.refresh_time) {
+                throw new Error('Tempo de atualização não encontrado');
+            }
+
+            return data.refresh_time;
+
         }catch (error) {
             console.error('Erro:', error);
             return null;
@@ -21,15 +27,11 @@ function useRefresh () {
 
     const triggerRefresh = async () => {
         try {
-            const response = await fetch(`${API_BASE_URL}/refresh`, {method: 'POST'}); // REVISAR ENDPOINT
+            const response = await fetch(`${API_BASE_URL}/refresh`, {method: 'POST', body: `${id}`}); // REVISAR ID
             if(!response.ok){
-                throw new Error('Erro ao acionar a atualização de dados');
+                throw new Error('Erro ao atualizar dados');
             }
 
-            const data = await response.json();
-
-            // o que fazer com os dados recebidos??
-            return data; // ou o que for necessário
         }catch(error){
             console.error('Erro:', error);
             return null;
