3b16474246f6c1f774f61a73735f55e400c94645
Erick <erickcarpesmmc@gmail.com>
2025-04-11 13:14:06 -0300

    use refresh

--- src/hooks/useRefresh.ts
+++ src/hooks/useRefresh.ts
@@ -1,19 +1,20 @@
-import { get } from "http";
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 
-const API_BASE_URL = "http://localhost:8000"; // REVISAR URL 
+function useRefresh ({account_id}: {account_id: string}) {
 
-function useRefresh ({id}: {id: string}) {
+    
+
 
     const fetchNextRefreshTime = async () => {
         try{
-            const response = await fetch(`${API_BASE_URL}/refresh/:${id}`, {method: 'GET'}); // REVISAR ID
+            const response = await fetch(`${API_BASE_URL}/refresh/${account_id}`, {method: 'GET'}); // REVISAR ID
             if (!response.ok) {
                 throw new Error('Erro ao buscar o próximo tempo de atualização de dados');
             }
 
             const data = await response.json();
 
-            if (!data || !data.refresh_time) {
+            if (!data.next_refresh_time) {
                 throw new Error('Tempo de atualização não encontrado');
             }
 
@@ -27,7 +28,7 @@ function useRefresh ({id}: {id: string}) {
 
     const triggerRefresh = async () => {
         try {
-            const response = await fetch(`${API_BASE_URL}/refresh`, {method: 'POST', body: `${id}`}); // REVISAR ID
+            const response = await fetch(`${API_BASE_URL}/refresh`, {method: 'POST', body: `${account_id}`}); // REVISAR ID
             if(!response.ok){
                 throw new Error('Erro ao atualizar dados');
             }
@@ -37,8 +38,6 @@ function useRefresh ({id}: {id: string}) {
             return null;
         }
     }
-
     return null;
 }
-
 export default useRefresh;
