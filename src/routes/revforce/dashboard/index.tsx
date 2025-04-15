42df266f640fb6a51d2107c0fd6f9ff012386a5b
Felipe Valiati <felipe.valiati@edu.pucrs.br>
2025-04-15 18:06:58 -0300

    feat top bar

--- src/routes/revforce/dashboard/index.tsx
+++ src/routes/revforce/dashboard/index.tsx
@@ -1,9 +1,23 @@
+import IconButton from '@/components/IconButton'
+import { Button } from '@/components/ui/button'
 import { createFileRoute } from '@tanstack/react-router'
+import { Icon, icons, Plus } from 'lucide-react'
 
 export const Route = createFileRoute('/revforce/dashboard/')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/dashboard/"!</div>
+  return <div className='w-full h-full flex flex-row justify-end'>
+    <h1 className='text-2xl font-bold'>Dashboard</h1>
+    <div className='w-2/3'> </div>
+    <div className='flex flex-row gap-2 items-center'>
+      <IconButton icon={icons.Funnel} />
+      <IconButton icon={icons.RefreshCcw} />
+      <IconButton icon={icons.SquarePen} />
+      <Button className='text-white'>
+        <Plus /> New Chart
+      </Button>
+    </div>
+  </div>
 }
