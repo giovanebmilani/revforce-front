686e6374d5a363747169a8fdb182b6a2ec731883
Felipe Valiati <felipe.valiati@edu.pucrs.br>
2025-04-15 18:45:33 -0300

    feat grafico

--- src/routes/revforce/dashboard/index.tsx
+++ src/routes/revforce/dashboard/index.tsx
@@ -1,21 +1,74 @@
 import IconButton from '@/components/IconButton'
 import { Button } from '@/components/ui/button'
 import { createFileRoute } from '@tanstack/react-router'
-import { Icon, icons, Plus } from 'lucide-react'
+import { icons, Plus } from 'lucide-react'
+import { Bar, BarChart } from "recharts"
 
 export const Route = createFileRoute('/revforce/dashboard/')({
   component: RouteComponent,
 })
 
+import { ChartConfig, ChartContainer } from "@/components/ui/chart"
+import { Card, CardHeader, CardTitle } from '@/components/ui/card'
+ 
+const chartData = [
+  { month: "January", desktop: 186, mobile: 80 },
+  { month: "February", desktop: 305, mobile: 200 },
+  { month: "March", desktop: 237, mobile: 120 },
+  { month: "April", desktop: 73, mobile: 190 },
+  { month: "May", desktop: 209, mobile: 130 },
+  { month: "June", desktop: 214, mobile: 140 },
+]
+ 
+const chartConfig = {
+  desktop: {
+    label: "Desktop",
+    color: "#2563eb",
+  },
+  mobile: {
+    label: "Mobile",
+    color: "#60a5fa",
+  },
+} satisfies ChartConfig
+
 function RouteComponent() {
-  return <div className='w-full h-full flex flex-row justify-end'>
-    <h1 className='text-3xl font-bold'>Dashboard</h1>
-    <div className='w-3/4'> </div> {/* pra separar o dashboard do resto (chinelagem, ajude ages 3) */}
-    <div className='flex flex-row gap-2 items-center'>
-      <IconButton icon={icons.RefreshCcw} />
-      <Button className='text-white'>
-        <Plus /> New Chart
-      </Button>
+  return <div className='w-full h-full flex flex-col'>
+    <div className='flex flex-row justify-end'>
+      <h1 className='text-3xl font-bold'>Dashboard</h1>
+      <div className='w-3/4'> </div> {/* pra separar o dashboard do resto (chinelagem, ajude ages 3) */}
+      <div className='flex flex-row gap-2 items-center'>
+        <IconButton icon={icons.RefreshCcw} />
+        <Button className='text-white'>
+          <Plus /> New Chart
+        </Button>
+      </div>
+    </div>
+
+    <div className='flex flex-row gap-4 items-center'>
+      <Card>
+        <CardHeader className='border-b'>
+          <CardTitle className='flex flex-row justify-between' >
+            Chart
+            <Button variant={'ghost'} className='text-blue-500'>Ver mais {">"}</Button>
+          </CardTitle>
+          
+        </CardHeader>
+        <ChartContainer config={chartConfig} className="h-40 w-80">
+          <BarChart accessibilityLayer data={chartData}>
+            <Bar dataKey="desktop" fill="var(--color-desktop)" radius={4} />
+            <Bar dataKey="mobile" fill="var(--color-mobile)" radius={4} />
+          </BarChart>
+        </ChartContainer>
+      </Card>
+
+      <Card>
+        <ChartContainer config={chartConfig} className="h-40 w-80">
+          <BarChart accessibilityLayer data={chartData}>
+            <Bar dataKey="desktop" fill="var(--color-desktop)" radius={4} />
+            <Bar dataKey="mobile" fill="var(--color-mobile)" radius={4} />
+          </BarChart>
+        </ChartContainer>
+      </Card>
     </div>
   </div>
 }
