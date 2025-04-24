91100c2b11390229e920de4a9a6e332432889eb3
Felipe Valiati <felipe.valiati@edu.pucrs.br>
2025-04-24 19:58:46 -0300

    style format

--- src/routes/revforce/dashboard/index.tsx
+++ src/routes/revforce/dashboard/index.tsx
@@ -5,22 +5,22 @@ import { icons, Plus } from 'lucide-react'
 import { Area, AreaChart, Bar, BarChart, CartesianGrid, Cell, Line, LineChart, Pie, PieChart, PolarAngleAxis, PolarGrid, Radar, RadarChart, XAxis } from "recharts"
 
 export const Route = createFileRoute('/revforce/dashboard/')({
-    component: RouteComponent,
+  component: RouteComponent,
 })
 
-import {ChartConfig, ChartContainer, ChartTooltip, ChartTooltipContent} from "@/components/ui/chart"
-import {Card, CardHeader, CardTitle} from '@/components/ui/card'
-import {listCharts, Chart} from '@/api/listCharts'
+import { ChartConfig, ChartContainer, ChartTooltip, ChartTooltipContent } from "@/components/ui/chart"
+import { Card, CardHeader, CardTitle } from '@/components/ui/card'
+import { listCharts, Chart } from '@/api/listCharts'
 
 const chartConfig = {
-    desktop: {
-        label: "Desktop",
-        color: "#FFDF20",
-    },
-    mobile: {
-        label: "Mobile",
-        color: "#FDC700",
-    },
+  desktop: {
+    label: "Desktop",
+    color: "#FFDF20",
+  },
+  mobile: {
+    label: "Mobile",
+    color: "#FDC700",
+  },
 } satisfies ChartConfig
 
 function createChartComponent(chart: Chart) {
@@ -30,20 +30,20 @@ function createChartComponent(chart: Chart) {
         <BarChart accessibilityLayer data={chart.entries}>
           <ChartTooltip content={<ChartTooltipContent />} />
 
-                    { // nao me pergunte, copilot fez td
-                        Object.keys(chart.entries[0] || {}).filter(key => typeof chart.entries[0][key] !== 'string').map((key, index) => (
-                            <Bar
-                                key={key}
-                                dataKey={key}
-                                fill={index % 2 === 0 ? chartConfig.desktop.color : chartConfig.mobile.color}
-                                radius={4}
-                            />
-                        ))
-                    }
+          { // nao me pergunte, copilot fez td
+            Object.keys(chart.entries[0] || {}).filter(key => typeof chart.entries[0][key] !== 'string').map((key, index) => (
+              <Bar
+                key={key}
+                dataKey={key}
+                fill={index % 2 === 0 ? chartConfig.desktop.color : chartConfig.mobile.color}
+                radius={4}
+              />
+            ))
+          }
 
-                    {/*<Bar dataKey="value" fill="var(--color-desktop)" radius={4} />*/}
-                </BarChart>
-            </ChartContainer>
+          {/*<Bar dataKey="value" fill="var(--color-desktop)" radius={4} />*/}
+        </BarChart>
+      </ChartContainer>
 
     case "Pie":
       return <ChartContainer config={chartConfig} className="h-2/3 w-full mx-auto aspect-square pb-0 [&_.recharts-pie-label-text]:fill-foreground">
@@ -125,107 +125,107 @@ function createChartComponent(chart: Chart) {
           <ChartTooltip cursor={false} content={<ChartTooltipContent />} />
           {Object.keys(chart.entries[0] || {}).filter(key => typeof chart.entries[0][key] !== 'string').map((key, index) => (
             <>
-            <defs>
-              <linearGradient id="fillDesktop" x1="0" y1="0" x2="0" y2="1">
-                <stop
-                  offset="5%"
-                  stopColor="var(--color-desktop)"
-                  stopOpacity={0.8} />
-                <stop
-                  offset="95%"
-                  stopColor="var(--color-desktop)"
-                  stopOpacity={0.1} />
-              </linearGradient>
-            </defs>
-            <Area
+              <defs>
+                <linearGradient id="fillDesktop" x1="0" y1="0" x2="0" y2="1">
+                  <stop
+                    offset="5%"
+                    stopColor="var(--color-desktop)"
+                    stopOpacity={0.8} />
+                  <stop
+                    offset="95%"
+                    stopColor="var(--color-desktop)"
+                    stopOpacity={0.1} />
+                </linearGradient>
+              </defs>
+              <Area
                 dataKey={key}
                 type="natural"
                 fill="url(#fillDesktop)"
                 fillOpacity={0.4}
                 stroke="var(--color-mobile)"
                 stackId="a" />
-                </>
+            </>
           ))}
 
         </AreaChart>
       </ChartContainer>
-      case "Radar":
-        return <ChartContainer
-          config={chartConfig}
-          className="mx-auto aspect-square h-2/3 w-full"
-        >
-          <RadarChart data={chart.entries}>
-            <ChartTooltip
-              cursor={false}
-              content={<ChartTooltipContent indicator="line" />}
+    case "Radar":
+      return <ChartContainer
+        config={chartConfig}
+        className="mx-auto aspect-square h-2/3 w-full"
+      >
+        <RadarChart data={chart.entries}>
+          <ChartTooltip
+            cursor={false}
+            content={<ChartTooltipContent indicator="line" />}
+          />
+          <PolarAngleAxis dataKey="identifier" />
+          <PolarGrid />
+          {Object.keys(chart.entries[0] || {}).filter(key => typeof chart.entries[0][key] !== 'string').map((key, index) => (
+
+            <Radar
+              dataKey={key}
+              fill="var(--color-desktop)"
+              fillOpacity={0.6}
             />
-            <PolarAngleAxis dataKey="identifier" />
-            <PolarGrid />
-            {Object.keys(chart.entries[0] || {}).filter(key => typeof chart.entries[0][key] !== 'string').map((key, index) => (
-            
-              <Radar
+
+          ))}
+
+        </RadarChart>
+      </ChartContainer>
+    case "BarNegative":
+      return <ChartContainer config={chartConfig} className="h-2/3 w-full">
+        <BarChart accessibilityLayer data={chart.entries}>
+          <ChartTooltip content={<ChartTooltipContent />} />
+
+          { // nao me pergunte, copilot fez td
+            Object.keys(chart.entries[0] || {}).filter(key => typeof chart.entries[0][key] !== 'string').map((key, index) => (
+              <Bar
+                key={key}
                 dataKey={key}
-                fill="var(--color-desktop)"
-                fillOpacity={0.6}
+                fill={index % 2 === 0 ? chartConfig.desktop.color : chartConfig.mobile.color}
+                radius={4}
               />
-
-            ))}
-            
-          </RadarChart>
-        </ChartContainer>
-      case "BarNegative":
-        return <ChartContainer config={chartConfig} className="h-2/3 w-full">
-          <BarChart accessibilityLayer data={chart.entries}>
-            <ChartTooltip content={<ChartTooltipContent />} />
-
-            { // nao me pergunte, copilot fez td
-              Object.keys(chart.entries[0] || {}).filter(key => typeof chart.entries[0][key] !== 'string').map((key, index) => (
-                <Bar
-                  key={key}
-                  dataKey={key}
-                  fill={index % 2 === 0 ? chartConfig.desktop.color : chartConfig.mobile.color}
-                  radius={4}
-                />
-              ))
-            }
-          </BarChart>
-        </ChartContainer>
+            ))
+          }
+        </BarChart>
+      </ChartContainer>
   }
 }
 
 async function RouteComponent() {
-    const chartData = await listCharts() //puxa a lista de chats
+  const chartData = await listCharts() //puxa a lista de chats
 
-    return <div className='w-full h-full flex flex-col gap-4'>
-        <div className='flex flex-row justify-end border-b pb-3'>
-            <h1 className='text-3xl font-bold h-full items-center flex align-items'>Dashboard</h1>
-            <div className='w-3/4'></div>
-            {/* pra separar o dashboard do resto (chinelagem, ajude ages 3) */}
-            <div className='flex flex-row gap-2 items-center'>
-                <IconButton icon={icons.RefreshCcw}/>
-                <Button className='text-white cursor-pointer'>
-                    <Plus/> New Chart
-                </Button>
-            </div>
-        </div>
-
-        <div className='flex flex-row gap-4 items-center flex-wrap'>
-            {chartData.map((chart: Chart) => { //TODO: Make card header the same for every chart without having to duplicate the code
-                return <Card key={chart.chartId} className='w-10/31 h-70'>
-                    <CardHeader className='border-b'>
-                        <CardTitle className='flex flex-row items-center content-normal justify-between'>
-                            Chart
-                            <Link to="/revforce/dashboard/chartdetails/$chartId"
-                                  params={{chartId: chart.chartId}}>
-                                <Button variant={'ghost'} className='text-blue-500 cursor-pointer'>
-                                    Ver mais {">"}
-                                </Button>
-                            </Link>
-                        </CardTitle>
-                    </CardHeader>
-                    {createChartComponent(chart)}
-                </Card>
-            })}
-        </div>
+  return <div className='w-full h-full flex flex-col gap-4'>
+    <div className='flex flex-row justify-end border-b pb-3'>
+      <h1 className='text-3xl font-bold h-full items-center flex align-items'>Dashboard</h1>
+      <div className='w-3/4'></div>
+      {/* pra separar o dashboard do resto (chinelagem, ajude ages 3) */}
+      <div className='flex flex-row gap-2 items-center'>
+        <IconButton icon={icons.RefreshCcw} />
+        <Button className='text-white cursor-pointer'>
+          <Plus /> New Chart
+        </Button>
+      </div>
     </div>
+
+    <div className='flex flex-row gap-4 items-center flex-wrap'>
+      {chartData.map((chart: Chart) => { //TODO: Make card header the same for every chart without having to duplicate the code
+        return <Card key={chart.chartId} className='w-10/31 h-70'>
+          <CardHeader className='border-b'>
+            <CardTitle className='flex flex-row items-center content-normal justify-between'>
+              Chart
+              <Link to="/revforce/dashboard/chartdetails/$chartId"
+                params={{ chartId: chart.chartId }}>
+                <Button variant={'ghost'} className='text-blue-500 cursor-pointer'>
+                  Ver mais {">"}
+                </Button>
+              </Link>
+            </CardTitle>
+          </CardHeader>
+          {createChartComponent(chart)}
+        </Card>
+      })}
+    </div>
+  </div>
 }
