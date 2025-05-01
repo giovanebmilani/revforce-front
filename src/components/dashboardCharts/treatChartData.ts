adae474e1347e4ebb937fcd8df563062e969df6c
Felipe Valiati <felipe.cvaliati@gmail.com>
2025-05-01 11:55:08 -0300

    funcao de tratar dados

--- src/components/dashboardCharts/treatChartData.ts
+++ src/components/dashboardCharts/treatChartData.ts
@@ -1,3 +1,33 @@
-export function treatChartData(response: ChartResponse) {
-  
+// const chartData = [
+//     { month: "January", desktop: 186, mobile: 80 },
+//     { month: "February", desktop: 305, mobile: 200 },
+//     { month: "March", desktop: 237, mobile: 120 },
+//     { month: "April", desktop: 73, mobile: 190 },
+//     { month: "May", desktop: 209, mobile: 130 },
+//     { month: "June", desktop: 214, mobile: 140 },
+// ]
+
+import { ChartResponse } from "@/api/listCharts";
+
+interface ChartDataItem {
+    identifier: string;
+    [key: string]: number | string;
+}
+
+export function treatChartData(response: ChartResponse): ChartDataItem[] {
+    
+    const chartData: ChartDataItem[] = [];
+
+    response.data.forEach((item) => {
+        chartData.push({
+            identifier: item.date.toLocaleDateString("pt-BR", {
+                month: "2-digit",
+                day: "2-digit",
+                year: "2-digit",
+            }),
+            [item.device || ""]: item.value,
+        });
+    })
+    
+    return chartData
 }
