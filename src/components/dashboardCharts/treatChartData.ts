bc9123dba97afc721e4a2d6f27a4c48f727bc77f
Felipe Valiati <felipe.cvaliati@gmail.com>
2025-05-01 16:21:20 -0300

    style tirei uns comment

--- src/components/dashboardCharts/treatChartData.ts
+++ src/components/dashboardCharts/treatChartData.ts
@@ -1,12 +1,3 @@
-// const chartData = [
-//     { month: "January", desktop: 186, mobile: 80 },
-//     { month: "February", desktop: 305, mobile: 200 },
-//     { month: "March", desktop: 237, mobile: 120 },
-//     { month: "April", desktop: 73, mobile: 190 },
-//     { month: "May", desktop: 209, mobile: 130 },
-//     { month: "June", desktop: 214, mobile: 140 },
-// ]
-
 import { ChartResponse } from "@/api/listCharts";
 
 interface ChartDataItem {
@@ -15,7 +6,6 @@ interface ChartDataItem {
 }
 
 export function treatChartData(response: ChartResponse): ChartDataItem[] {
-    
     const chartData: ChartDataItem[] = [];
 
     response.data.forEach((item) => {
