d7dcad804b604526b0ebc03314fe02bbc7d0562a
Erick <erickcarpesmmc@gmail.com>
2025-05-03 14:56:18 -0300

    mais um

--- src/components/TabChat.tsx
+++ src/components/TabChat.tsx
@@ -2,24 +2,26 @@ import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 
 interface TabChatProps {
-    onClick: () => void;
-    value: string;
-    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-    children: React.ReactNode
+  onClick: () => void;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  tab: React.ReactNode;
 }
 
-    function TabChat({onClick, value, onChange, children}: TabChatProps) {
-    return (
-        <div className="flex flex-col w-full h-50 p-3 border-1 rounded-lg">
-            <div className="flex flex-col gap-2 h-full overflow-scroll">
-                {children}
-            </div>
-            <div className="flex mt-auto gap-3">
-                <Input placeholder="Write here" value={value} onChange={onChange}></Input>
-                <Button onClick={onClick}>Send</Button>
-            </div>
-        </div>
-    );
+function TabChat({ onClick, value, onChange, tab }: TabChatProps) {
+  return (
+    <div className="flex flex-col w-full h-full p-3 border-1 rounded-lg">
+      <div className="flex flex-col gap-2 h-full overflow-scroll">{tab}</div>
+      <div className="flex mt-auto gap-3">
+        <Input
+          placeholder="Write here"
+          value={value}
+          onChange={onChange}
+        ></Input>
+        <Button onClick={onClick}>Send</Button>
+      </div>
+    </div>
+  );
 }
 
 export default TabChat;
