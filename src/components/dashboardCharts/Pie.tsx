4b60bfb8a15d770c765ff6feffd8ce7924ec189d
Felipe Valiati <felipe.cvaliati@gmail.com>
2025-05-04 14:27:40 -0300

    fix pie chart

--- src/components/dashboardCharts/Pie.tsx
+++ src/components/dashboardCharts/Pie.tsx
@@ -1,36 +1,55 @@
-import { Area, AreaChart, Bar, BarChart, CartesianGrid, Cell, LabelList, Line, LineChart, Pie, PieChart, PolarAngleAxis, PolarGrid, Radar, RadarChart, XAxis } from "recharts"
-import { ChartConfig, ChartContainer, ChartTooltip, ChartTooltipContent } from "@/components/ui/chart"
+import { Pie, PieChart } from "recharts"
+import { ChartConfig, ChartContainer, ChartLegend, ChartLegendContent, ChartTooltip, ChartTooltipContent } from "@/components/ui/chart"
 import { ChartResponse } from "@/api/listCharts"
-import { treatChartData } from "./treatChartData"
 
+interface PieChartDataItem {
+    identifier: string;
+    value: number;
+    fill: string;
+}
+
+function treatDataForPie(response: ChartResponse): PieChartDataItem[] {
+    const pieDict: { [key: string]: number } = {};
+
+    response.data.forEach((value) => {
+        if (typeof response.chart.segment === "string") {
+            const key = String(value[response.chart.segment] ?? "unknown");
+            if (key in pieDict) {
+                pieDict[key] += value.value;
+            } else {
+                pieDict[key] = value.value;
+            }
+        }
+    })
+
+    const pieData: PieChartDataItem[] = []
+    Object.keys(pieDict).forEach((value) => {
+        pieData.push({
+            identifier: value,
+            value: pieDict[value],
+            fill: `var(--color-${value})`,
+        })
+    })
+
+    console.log("pieData", pieData)
+
+    return pieData
+}
 
 export function createDashboardPieChartComponent(response: ChartResponse, chartConfig: ChartConfig) {
-    const entries = treatChartData(response);
-    
-    return <ChartContainer config={chartConfig} className="h-3/5 w-full mx-auto aspect-square pb-0 [&_.recharts-pie-label-text]:fill-foreground">
+    const entries = treatDataForPie(response);
+
+    return <ChartContainer config={chartConfig} className="h-5/6 w-full mx-auto pb-0 [&_.recharts-pie-label-text]:fill-foreground">
         <PieChart>
-          <ChartTooltip content={<ChartTooltipContent hideLabel />} />
-          {
-            Object.keys(entries[0] || {}).filter(key => typeof entries[0][key] !== 'string').map((key) => (
-              <Pie
-                data={entries}
-                key={key}
-                dataKey={key}
-                fill="#8884d8" // Default color
-                label
-                nameKey="identifier"
-              >
-                {
-                  entries.map((_, index) => (
-                    <Cell
-                      key={`cell-${index}`}
-                      fill={index % 2 === 0 ? chartConfig.desktop.color : chartConfig.mobile.color}
-                    />
-                  ))
-                }
-              </Pie>
-            ))
-          }
+            <Pie data={entries} dataKey="value" />
+            <ChartLegend
+                content={<ChartLegendContent nameKey="identifier" />}
+                className="-translate-y-2 flex-wrap gap-2 [&>*]:basis-1/4 [&>*]:justify-center"
+            />
+            <ChartTooltip
+                cursor={false}
+                content={<ChartTooltipContent hideLabel />}
+            />
         </PieChart>
     </ChartContainer>
 }
