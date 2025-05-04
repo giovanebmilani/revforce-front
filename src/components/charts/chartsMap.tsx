dfb13bc555b5152aa848e34894ce1d295b5f04ad
Pablo Montiel <pablopmontiel98@gmail.com>
2025-05-04 15:36:12 -0300

    pasta de gráficos, chartsMap e fixes

---
+++ src/components/charts/chartsMap.tsx
@@ -0,0 +1,17 @@
+import {Component as AreaStacked} from './areaStacked';
+import {Component as BarHoriz} from './barHoriz';
+import {Component as BarMult} from './barMult';
+import {Component as BarVertic} from './barVertic';
+import {Component as LineMult} from './lineMult';
+import {Component as LineSimple} from './lineSimple';
+import {Component as PizzaLegend} from './pizzaLegend';
+
+export const chartsMap = {
+    areaStacked: AreaStacked,
+    barHoriz: BarHoriz,
+    barMult: BarMult,
+    barVertic: BarVertic,
+    lineMult: LineMult,
+    lineSimple: LineSimple,
+    pizzaLegend: PizzaLegend,
+  };
