f9b748a3c238514c1a38fbcf81f3c7819ae64808
Augusto Knob <gustoknob@gmail.com>
2025-05-26 16:21:45 -0300

    TabEvents finished

--- src/components/TabEvents.tsx
+++ src/components/TabEvents.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef } from "react";
 import { cn } from "@/lib/utils";
+import { Button } from "./ui/button";
 
 interface TabEventsProps {
   children: React.ReactNode;
@@ -9,13 +10,19 @@ interface TabEventsProps {
 const TabEvents: React.FC<TabEventsProps> = ({ children, className }) => {
   const scrollRef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    scrollRef.current?.scrollIntoView({ behavior: "smooth" });
-  }, [children]);
-
   return (
-    <div className={cn("h-full max-h-full flex flex-col", className)}>
-      <div className="flex-1 overflow-y-auto space-y-4 pr-2">
+    <div
+      className={cn(
+        "flex flex-col h-full",
+        "min-h-[max(300px,calc(100vh-600px))]",
+        "max-h-[calc(100vh-600px)]",
+        className
+      )}
+    >
+      <div className="flex justify-center mb -4">
+        <Button variant="pointer">Novo Evento</Button>
+      </div>
+      <div className="flex-1 overflow-x-auto overflow-y-auto pr-2">
         {children}
         <div ref={scrollRef} />
       </div>
